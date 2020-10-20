32e230b944b50773f698b6fa275eaf4fd96957fd
demyansson <idcadifi@gmail.com>
2020-10-20 20:38:38 +0300

    header

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Home from "./pages/Home";
+import Header from "./Header";
 
 export const App = () => {
   return (
     <div>
+      <Header/>
       <main>
         <Home/>
       </main>
---
+++ imports/ui/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-item header-logo">
+        <div className="header-logo-img">
+          <img src="/img/logo-small.png" alt=""/>
+        </div>
+        <div className="header-logo-title">
+          MEEKER
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
